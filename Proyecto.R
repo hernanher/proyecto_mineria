e2c46df010edbc95a7e69529f6039a2e5050219e
ImVicente <101204565+ImVicente@users.noreply.github.com>
2022-04-30 21:16:05 -0400

    Listo segundo modelo (GMM)
    Creo que esto ya está listo como entrega número 1, ahora falta simplemente hacer la 2 que sería copiar y pegar el modelo que hayamos escogido (Ni idea como escogerlo)

--- Proyecto.R
+++ Proyecto.R
@@ -1,4 +1,4 @@
-pacman::p_load(mclust, e1071, cluster, flexclust, factoextra, magrittr,dbscan, tidyverse, Rtsne, dplyr)
+pacman::p_load(mclust, e1071, cluster, flexclust, factoextra, magrittr,dbscan, tidyverse, Rtsne, dplyr,factoextra)
 
 #Escogemos Semilla
 set.seed(43)
@@ -20,15 +20,15 @@ data_clean <- data3%>%
 
 #Separamos data con variables que identifiquen la canción
 data_id<-data_clean%>%
-  select(track_name,duration_ms,track_number)%>%
+  select(track_name,duration_ms,track_number,artist_name)%>%
   unique()
 
 #Separamos data con variables que nos entreguen datos de la canción (Todos númericos)
 data_num <- data_clean%>%
-select(danceability,energy,loudness,mode,speechiness,acousticness,liveness,valence,track_number)%>% 
+  select(danceability,energy,instrumentalness,tempo,loudness,speechiness,acousticness,liveness,valence,track_number)%>% 
   unique()
 
-#Creamos el data Tsne para realizar el módelo Dbscan
+#Creamos el data Tsne para realizar el modelo Dbscan y modelo GMM
 data_tsne <- data_num %>% 
   unique() %>% 
   Rtsne() %>% 
@@ -40,45 +40,21 @@ ggplot(data_tsne, aes(V1, V2)) +
   geom_point(alpha = 0.5)
 
 
-#Gráficamos Knn para escoger eps, a lo cual notamos que el punto de inflexíon se aproxima en 2
-kNNdistplot(data_tsne, k = 4)
-abline(h=2, col = "red", lty = 2)
-dev.off() 
-
-#Creamos el modelo Dbscan con el parametro escogido
-modelo_dbscan <- dbscan(data_tsne, eps = 2, minPts = 9)
-
-#Gráficamos Dbscan
-ggplot(data_tsne, aes(V1, V2, col = factor(modelo_dbscan$cluster))) + 
-  geom_point(alpha = 0.5) +
-  theme(legend.position = "none")
-
-#Vemos cuantos cluster tenemos
-modelo_dbscan$cluster %>% max()
-max(modelo_dbscan$cluster)
-
-#Creamos data frame de los clusters
-clusters= as.data.frame(modelo_dbscan$cluster)
-
-#Unimos la data_id con la recién creada clusters para conocer el nombre, duración 
-#Y cluster correspondiente a la base
-base <- data_id %>% 
-  mutate(cluster = modelo_dbscan$cluster) %>%
-  group_by(cluster)
+#========================================== Manipulación De Usuario ==============================================
 
 #Le preguntamos al usuario el nombre de la canción que desea utilizar
 #Y comprobamos que efectivamente se encuentre en la muestra
-#Ejemplo: "Train"
+#Ejemplo: "Train" o "26j26hmfGEyzWo222T8Eqx"
 validar=0
 i=1
-song <- readline(prompt = "Ingrese el nombre de la canción: ")
+song <- readline(prompt = "Ingrese el nombre o ID de la canción: ")
 while(validar!=1){
   for (i in 1:nrow(data3)) {
-    if(song==data3[i,27]) {
+    if(song==data_clean[i,27] | song==data_clean[i,19]) {
       validar=1
       #Con la variable indice, guardamos la posición de la canción, lo cual servirá para saber su cluster 
       indice=i
-      cat("La canción",song,"está en la muestra de la base de datos")
+      cat("La canción o Id",song,"está en la muestra de la base de datos")
       break
     }
   }
@@ -87,79 +63,127 @@ while(validar!=1){
   }
 }
 
-#Con la variable cltr guardamos el cluster de la canción ingresada
-tiempo=0
-j=1
-cltr=clusters[indice,1]
+
+
+
+#========================================== Función Crear Playlist ==============================================
 
 #Creamos dataframe con las canciones
-lista=data.frame(Cancion=character(),Duracion_Acumulado=numeric())
+lista=data.frame(Autor_Grupo=character(), Cancion=character(),Duracion_Acumulado=numeric())
 #Notamos que 10800000 milisegundos son 3 horas, por lo que el while se detendrá
 #Una vez haya superado ese tiempo
 #Pd: Si la muestra tomada obtiene clusters que no logren satisfacer todo el tiempo,
 #Las canciones se comenzarán a repetir hasta cumplir las 3 horas, siendo todas ellas,
 #Del mismo cluster
-playlist <- function(data,tiempo, j, cltr, lista){
+
+playlist <- function(Base,tiempo, j, cltr, lista){
   while(tiempo<=10800000){
-    for (j in 1:nrow(data)) {
-      if(cltr==data[j,4]) {
+    for (j in 1:nrow(Base)) {
+      if(cltr==Base[j,5]) {
         if (tiempo >=10800000){
           break
         }
-        tiempo=tiempo+data[j,2]
-        cancion=data[j,1]
-        vector=c(cancion,tiempo)
+        tiempo=tiempo+Base[j,2]
+        cancion=Base[j,1]
+        autor=Base[j,4]
+        vector=c(autor,cancion,tiempo)
         lista=rbind(lista,vector)
       }
+      
     }
   }
   return(lista)
 }
-playKNN<- playlist(base,tiempo,j,cltr,lista)
-
-view(playKNN)
-
-data_PCAproy <- data_clean %>% 
-  prcomp(scale. = TRUE) %>% 
-  predict() %>% 
-  .[,1:2] %>% 
-  as_tibble()
-
-ggplot(data_PCAproy, aes(PC1, PC2)) + 
-  geom_point(alpha = 0.5)
-
-data_tsne <- data_clean %>% 
-  unique() %>% 
-  Rtsne() %>% 
-  .$Y %>% 
-  as.data.frame()
-# visualizo tsne
-ggplot(data_tsne, aes(V1, V2)) + 
-  geom_point(alpha = 0.5)
+#==================================================DB-SCAN========================================================
 
 
-# visualizo grafico de kNN
+#Gráficamos Knn para escoger eps, a lo cual notamos que el punto de inflexíon se aproxima en 1,7
 kNNdistplot(data_tsne, k = 4)
-abline(h=2, col = "red", lty = 2)
+abline(h=1.7, col = "red", lty = 2)
 dev.off() 
 
-# hago un DBScan con parametro observado
-modelo_dbscan <- dbscan(data_tsne, eps = 2, minPts = 9)
+#Creamos el modelo Dbscan con el parametro escogido
+modelo_dbscan <- dbscan(data_tsne, eps = 1.7, minPts = 7)
 
-#visualizo
+#Gráficamos Dbscan
 ggplot(data_tsne, aes(V1, V2, col = factor(modelo_dbscan$cluster))) + 
-  geom_point(alpha = 0.5) +
-  theme(legend.position = "none")
-
-modelo_dbscan$cluster %>% max()
-
-max(modelo_dbscan$cluster)
-
-modelo_dbscan$cluster %>% unique() %>% length()
+  geom_point(alpha = 0.5) 
 
 
+#Vemos cuantos cluster tenemos
+n_clusters= (modelo_dbscan$cluster %>% max())+1
 
-clusters= as.data.frame(modelo_dbscan$cluster)
-cat("La canción",song,"pertenece al cluster",clusters[indice,1])
+#Creamos data frame de los clusters
+clusters_DbScan= as.data.frame(modelo_dbscan$cluster)
+
+#Unimos la data_id con la recién creada clusters para conocer el nombre, duración 
+#Y cluster correspondiente a la base
+Base_DbScan <- data_id %>% 
+  mutate(clusters_DbScan = modelo_dbscan$cluster) %>%
+  group_by(clusters_DbScan)
+
+#Indicamos el Cluster de la canción
+tiempo=0
+j=1
+cltr_DbScan=clusters_DbScan[indice,1]
+#Utilizamos función crear playlist para el modelo DbScan
+
+Playlist_DbScan<- playlist(Base_DbScan,tiempo,j,cltr_DbScan,lista)
+
+view(Playlist_DbScan)
+
+#========================================================= GMM =============================================================
+
+# Usamos función vista en clases para graficar cualquier version del modelo
+gmm_func <- function(data, k, model, threshold=NULL){
+  #Se crea modelo con los parametros dados
+  model_gmm <- Mclust(data, G=k, modelNames = model)
+  
+  # si el parametro es nulo usar clasificacion, de lo contrario usar la maxima membresia siempre que supere el umbral
+  if(is.null(threshold)){
+    fuzz <- model_gmm$classification
+  } else {
+    fuzz <- apply(model_gmm$z,1, function(x) ifelse(max(x) > threshold, which(x==max(x)),0))
+  }
+  
+  #Grafica
+  ggplot(data[fuzz != 0,]) +
+    geom_point(aes(V1,V2, color=factor(fuzz[fuzz != 0]))) +
+    geom_point(data=data[fuzz == 0,], aes(V1,V2), alpha = 0.5, size=0.8) +
+    ggtitle(paste0(round(nrow(data[fuzz == 0,])/nrow(data)*100,0),"% de los datos son cluster"))
+  
+}
+#Probamos un modelo con los mismos clusters que en dbscan y ya notamos diferencias con clusters más unidos y notorios
+gmm_func(data_tsne, n_clusters, "VEE", 0.7)
+
+
+#Calculamos un modelo con todas las opciones posibles
+model_all <- Mclust(data_tsne, G=1:30)
+
+#Visualizamos
+plot(model_all, what = "BIC")
+
+#Graficamos el Optimo
+ggplot(data_tsne) + 
+  aes(x=V1, y=V2, color=factor(model_all$classification)) + 
+  geom_point(alpha=0.5) 
+
+#Creamos data frame de los clusters
+clusters_GMM= as.data.frame(model_all$classification)
+
+#Unimos la data_id con la recién creada clusters para conocer el nombre, duración, autor y
+#Cluster correspondiente a la base
+Base_Gmm <- data_id %>% 
+  mutate(clusters_GMM = model_all$classification) %>%
+  group_by(clusters_GMM)
+
+tiempo=0
+j=1
+#Indicamos el Cluster de la canción
+cltr_Gmm=clusters_GMM[indice,1]
+
+#Utilizamos función crear playlist para el modelo gmm
+Playlist_GMM<- playlist(Base_Gmm,tiempo,j,cltr_Gmm,lista)
+view(Playlist_GMM)
 
 
